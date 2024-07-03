e123df2fab03c96cabf5470f923f62ac573abe04
dracc <ssrinet10@gmail.com>
2024-07-04 01:43:30 +0530

    trying to add google api into current cli

--- bin/translate.js
+++ bin/translate.js
@@ -3,12 +3,12 @@ const yargs=require("yargs")
 const tunnel=require('tunnel')
 const chalk=require('chalk')
 var fs=require('fs')
-const createHttpProxyAgent =require('http-proxy-agent')
+const {Translate}=require('@google-cloud/translate').v2;
 const { hideBin }=require('yargs/helpers')
-const {translate}=require('@vitalets/google-translate-api')
+const {translate}=require('@vitalets/google-translate-api');
 
-const agent = createHttpProxyAgent('http://103.152.112.162:80');
 const usage="$0 -l <language> -s <sentence>";
+const translater=new Translate();
 const options=yargs
                 .usage(usage)
                 .option("l",{alias:"language",demandOption:false,describe:'language to be converted into',type:'string'})
@@ -21,24 +21,27 @@ const sentence=argv.s;
 const translator=async ()=>{
     let res;
     try{
-     res= await translate(sentence,{to:language,fetchOptions:{agent}})
+     res= await translate(sentence,{to:language})
     }
     catch(error){
         console.log(`Invalid api response \nCheck language shorthand \n${error}`);
         return null;
     }
+    const [languages]=await translater.getLanguages(language);
+    languages.forEach(language=>console.log(language));
     if(res && res.text){
-    console.log(chalk.keyword('blue')(res.text))
-    console.log(res.from.language.value)
+    console.log(`${chalk.keyword('green')('Translated:')} ${chalk.keyword('blue')(res.text)}`)
     try{
     fs.appendFile('translateFile.txt','converted',(err)=>{
         if(err) throw err;
     })}
     catch(err){
         console.log(`error in writing file ${err}`)
+
     }
-    console.log(chalk.keyword('green')(`Language: ${argv.l}`))
-    console.log(chalk.keyword('green')(`Sentence: ${argv.s}`))
+    console.log(`${chalk.keyword('green')('Translated:')} ${chalk.keyword('blue')(argv.l)}`)
+    console.log(`${chalk.keyword('green')('Translated From:')} ${chalk.keyword('blue')(res.raw.src)}`)
+    console.log(chalk.keyword('green')(`Sentence Translated: ${argv.s}`))
 }else{
     console.log('No translation result')
 }
